6f6c3bf09bbd0d43bf79cc160d94f6e50fa09770
TeeWrath <subroto.2003@gmail.com>
2023-10-01 17:18:01 +0530

    modifications

--- datatype.js
+++ datatype.js
@@ -1,14 +1,13 @@
 
 // JavaScript has 8 Datatypes
 
-// 1. String
-// 2. Number
-// 3. Bigint
-// 4. Boolean
-// 5. Undefined
-// 6. Null
-// 7. Symbol
-// 8. Object
+// 1. String  "Hello world", 'Hello world', `Hello world`
+// 2. Number  123, 12.3 , 1.23e10  
+// 3. Bigint  9007199254740991n
+// 4. Boolean  true, false
+// 5. Undefined     
+// 6. Null 
+// 7. Object {"name" : "John Doe"}
 // The Object Datatype
 
 
@@ -16,4 +15,3 @@
 
 // 1. An object
 // 2. An array
-// 3. A date
